9e05caaf47eb2ba6a22d37df27f4d166f3be85ca
Kuniaki IGARASHI <igaiga@gmail.com>
2012-08-27 07:21:03 +0900

    add twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,21 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-#  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  provider :facebook, '147633245378151', '5af7ded1a1b671bb7593df697ace719f'
-  # setting - basic - Website with Facebook Login
+  # twitter
+  #   https://dev.twitter.com/apps
+  #   setting - call back URL : http://my.herokuapp.com/
+  provider :twitter, OmniauthSettings::TWITTER_CONSUMER_KEY, OmniauthSettings::TWITTER_CONSUMER_SECRET
+
+  # facebook
+  #   https://developers.facebook.com/apps
+  #   setting - basic - Website with Facebook Login
   #   Site URL: http://my.herokuapp.com/
+  provider :facebook, OmniauthSettings::FACEBOOK_APP_KEY, OmniauthSettings::FACEBOOK_APP_SECRET
 end
+
+# Careate a config/initializers/omniauth_settings.rb like below.
+# class OmniauthSettings
+#   TWITTER_CONSUMER_KEY = 'CONSUMER_KEY'
+#   TWITTER_CONSUMER_SECRET = 'CONSUMER_SECRET'
+#   FACEBOOK_APP_KEY = 'APP_KEY'
+#   FACEBOOK_APP_SECRET = 'APP_SECRET'
+# end
+
